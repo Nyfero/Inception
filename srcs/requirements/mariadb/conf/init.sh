21a0a98648d3c7dcf36e7a6112cc8471f08491a9
Nyfero <gaetan.sap@gmail.com>
2022-09-14 14:06:01 +0200

    ready to go

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -9,21 +9,13 @@ done
 #	Create 'wordpress' database
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 
-#	Remove 'test' database
-mysql -u root -e "DROP DATABASE IF EXISTS 'test';"
-
 #	Create admin for wordpress database
 mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'%';"
 
-# Reload access right
-mysql -u root -e "FLUSH PRIVILEGES;"
-
 # Create user for wordpress database
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-
-mysql -u root -e "FLUSH PRIVILEGES;"
 
 #	Remove root and anonymous user
 mysql -u root -e "DELETE FROM mysql.user WHERE user='';"
--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -1,25 +1,22 @@
-
-#------------DL & EXTRACT WORDPRESS------------#
+# Download wp
 mkdir -p /var/www/html
 cd /var/www/html/
-wp core download --allow-root  # you can do command even if you are root
+wp core download --allow-root
 
-#------------WAITING FOR MARIADB------------#
+# Waiting for mdb
 while ! mysqladmin -hmariadb -u${MYSQL_USER} -p${MYSQL_USER_PASSWORD} ping; do
 	sleep 2
 done
 
-#------------CONFIG FILE------------#
+# Create new wp-config.php and check database
 wp config create	--dbname=${MYSQL_DATABASE} \
 					--dbuser=${MYSQL_USER} \
 					--dbpass=${MYSQL_USER_PASSWORD} \
 					--dbhost=mariadb:3306 \
-					--dbcharset="utf8" \
 					--dbcollate="utf8_general_ci" \
 					--allow-root
 
-#------------INSTALL WORDPRESS------------#
-# the famous 5 minute install in 0.0001s
+# Create wp table in database
 wp core install --url=${WP_URL} \
 				--title="${WP_TITLE}" \
 				--admin_user=${WP_ADMIN} \
@@ -27,15 +24,10 @@ wp core install --url=${WP_URL} \
 				--admin_email=${WP_ADMIN_MAIL} \
 				--allow-root
 
-#------------USER CREATION------------#
+# Create user
 wp user create	${WP_USER} ${WP_USER_MAIL} \
 				--role=author \
 				--user_pass=${WP_USER_PASSWORD} \
 				--allow-root
 
 exec "$@"
-
-# exec "$@" is typically used to make the entrypoint a pass through that then runs the docker command.
-# It will replace the current running shell with the command that "$@" is pointing to.
-# By default, that variable points to the command line arguments.
-# To simplify : First it will exec everything and after do the CMD in the dockerfile
